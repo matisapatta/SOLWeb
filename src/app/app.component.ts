fcd241955ec5d9369912215c29375e8f3fe30645
Alex Otero <alex.otero@globant.com>
2018-05-10 00:25:35 -0300

    Add basic FE UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  nombreSala: string = 'nombre default';
+  nombreCrearSala: string;
+  sala = {};
+
+  constructor(private http: HttpClient) {}
+
+  guardarSala() {
+    this.http.get('/api/sala/' + this.nombreSala).subscribe((sala) => {
+        this.sala = sala;
+    })
+  }
+
+  crearSala() {
+    this.http.post('/api/sala/' + this.nombreCrearSala, {}).subscribe((response) => {
+      debugger;
+      alert(response);
+    });
+  }
 }
